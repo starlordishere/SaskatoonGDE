0851146a911d061db5c4b3058edf9d2ee45a1fec
sulavrrijal <36382163-sulavrrijal@users.noreply.replit.com>
2024-11-07 14:57:05 +0000

    Restored to '0fe2b28bcf250b2576eaf8f651fa15ec4399ed0a'
    Replit-Restored-To:0fe2b28bcf250b2576eaf8f651fa15ec4399ed0a

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
 from app import app, socketio
 
 if __name__ == "__main__":
-    # Use standard port 5000 for deployment
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=5000)
