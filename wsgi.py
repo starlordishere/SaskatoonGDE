33855ff4f49eb3b0afcee2680be598f1509ba0e0
sulavrrijal <36382163-sulavrrijal@users.noreply.replit.com>
2024-11-07 02:23:49 +0000

    Agent query: Is the website accessible and functioning correctly with all features (contact form, chat, blog) working as expected?

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,17 @@ from app import app as application
 application.config.update(
     ENV='production',
     DEBUG=False,
-    PREFERRED_URL_SCHEME='https'
+    PREFERRED_URL_SCHEME='https',
+    SERVER_NAME='saskatoongaragedoorexpets.ca',
+    SQLALCHEMY_ENGINE_OPTIONS={
+        'pool_size': 10,
+        'pool_recycle': 300,
+        'pool_pre_ping': True,
+        'max_overflow': 5,
+        'connect_args': {
+            'sslmode': 'require'
+        }
+    }
 )
 
 # Configure logging for production
@@ -36,26 +46,22 @@ application.logger.addHandler(file_handler)
 application.logger.setLevel(logging.INFO)
 application.logger.info('Saskatoon Garage Door Experts startup')
 
-# Add HSTS header
+# Add HSTS and security headers
 @application.after_request
 def add_security_headers(response):
-    if application.config['STRICT_TRANSPORT_SECURITY']:
-        hsts_header = 'max-age={}'.format(application.config['STRICT_TRANSPORT_SECURITY_MAX_AGE'])
-        if application.config['STRICT_TRANSPORT_SECURITY_INCLUDE_SUBDOMAINS']:
-            hsts_header += '; includeSubDomains'
-        if application.config['STRICT_TRANSPORT_SECURITY_PRELOAD']:
-            hsts_header += '; preload'
-        response.headers['Strict-Transport-Security'] = hsts_header
-
-    # Add CORS headers
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = ', '.join(application.config['CORS_HEADERS'])
+    # HSTS header
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload'
+    
+    # Security headers
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
     
     # Cache control for static files
     if request.path.startswith('/static/'):
-        response.cache_control.max_age = application.config['STATIC_CACHE_TIMEOUT']
+        response.cache_control.max_age = 2592000  # 30 days
         response.cache_control.public = True
+        response.headers['Vary'] = 'Accept-Encoding'
     
     return response
 
@@ -68,8 +74,7 @@ def before_request():
         return redirect(url, code=301)
 
     # Redirect non-www to www
-    if (application.config.get('FORCE_WWW') and 
-        request.host.startswith('saskatoongaragedoorexpets.ca') and 
+    if (request.host.startswith('saskatoongaragedoorexpets.ca') and 
         not request.host.startswith('www.')):
         url = request.url.replace('://', '://www.', 1)
         return redirect(url, code=301)
